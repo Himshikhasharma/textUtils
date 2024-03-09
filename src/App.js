d6f30ef609dc787a893e5ca83d4524fae4f57699
Himshikha <sharmahimshikha18@gmail.com>
2024-03-10 04:05:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                About
+                About Us
               </a>
               <ul className="dropdown-menu">
                 <li><a className="dropdown-item" href="/"> Application</a></li>
